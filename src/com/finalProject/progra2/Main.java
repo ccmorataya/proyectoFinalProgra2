56fe47e28675e42501051c4b4ea34f01b74db554
Cristian Morataya <cris.morataya@gmail.com>
2016-11-18 19:13:12 -0600

    Add the handling of the program using Scanner and the menus

--- src/com/finalProject/progra2/Main.java
+++ src/com/finalProject/progra2/Main.java
@@ -7,41 +7,65 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.csvreader.CsvReader;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
+        boolean isAlive = true;
+        Scanner scann = new Scanner(System.in);
+        int i = 0;
+        List<Usuario> usuarios = new ArrayList<>();
 
-            List<Usuario> usuarios = new ArrayList<>();
+        while(isAlive) {
+            Menu.show();
+            i = Integer.parseInt(scann.nextLine());
 
-            Path filePath = Paths.get(System.getProperty("user.dir") + "/usuarios_import.csv");
-            System.out.println(filePath);
-            CsvReader usuarios_import = new CsvReader(filePath.toString());
-            usuarios_import.readHeaders();
+            if (i == 1) {
+                try {
+                    Path filePath = Paths.get(System.getProperty("user.dir") + "/usuarios_import.csv");
+                    System.out.println(filePath);
+                    CsvReader usuarios_import = new CsvReader(filePath.toString());
+                    usuarios_import.readHeaders();
 
-            while (usuarios_import.readRecord())
-            {
-                String curso = usuarios_import.get(0);
-                String carne = usuarios_import.get(1);
-                String alumno = usuarios_import.get("Alumno");
-                String nota = usuarios_import.get("Nota");
+                    while (usuarios_import.readRecord()) {
+                        String curso = usuarios_import.get(0);
+                        String carne = usuarios_import.get(1);
+                        String alumno = usuarios_import.get("Alumno");
+                        String nota = usuarios_import.get("Nota");
 
-                usuarios.add(new Usuario(curso, carne, alumno, nota));
+                        usuarios.add(new Usuario(curso, carne, alumno, nota));
+                    }
+
+                    usuarios_import.close();
+
+//                for (Usuario us : usuarios) {
+//
+//                    System.out.println(us.getCurso() + " : " + us.getCarne() + " - "
+//                            + us.getAlumno() + " - " + us.getNota());
+//                }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+            else if (i == 2){
+                Menu.find();
 
-            usuarios_import.close();
+                i = Integer.parseInt(scann.nextLine());
 
-            for(Usuario us : usuarios){
-
-                System.out.println(us.getCurso() + " : " + us.getCarne() + " - "
-                        + us.getAlumno() + " - " + us.getNota());
+                if (i == 1){
+                    System.out.println("Search all words\n");
+                }
+                else if (i == 2){
+                    System.out.println("Search any word\n");
+                }
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            else if (i == 0){
+                isAlive = false;
+            }
         }
 
     }
--- src/com/finalProject/progra2/Menu.java
+++ src/com/finalProject/progra2/Menu.java
@@ -1,7 +1,16 @@
 package com.finalProject.progra2;
 
-/**
- * Created by user on 18/11/2016.
- */
-public class Menu {
+class Menu {
+    static void show(){
+        System.out.println("Manejador de Notas de alumnos con archivos CSV");
+        System.out.println("Ingresa la opcion que deseas:");
+        System.out.println("1. Cargar Archivos");
+        System.out.println("2. Buscar Archivos\n");
+        System.out.println("0. Salir del sistema");
+    }
+
+    static void find(){
+        System.out.println("1. Busqueda por todas las palabras ingresadas");
+        System.out.println("2. Busqueda por cualquier palabra ingresada");
+    }
 }
